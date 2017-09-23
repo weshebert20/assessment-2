cd4d0e31f9f2122dcb6119eefea34611ed1c60fe
weshebert20 <weshebert20@gmail.com>
2017-09-23 16:56:59 -0600

    set a placement for winner

--- js/app.js
+++ js/app.js
@@ -5,9 +5,9 @@ $( document).ready(function() {
     	if(evt.which == 80){
             $("#playerOne").animate({left: "+=40"},100);   
         } 
-// declare winner by setting a position and alert player 1 won
+// declare winner by setting a position and set html player 1 won
         if($("#playerOne").position().left > 1100){
-            alert("Player One Winner");
+            $(".winnerPlacement").html("Player One Wins");
         }
     });
 
@@ -16,12 +16,12 @@ $( document).ready(function() {
     	if(evt1.which == 81){
             $("#playerTwo").animate({left: "+=40"},100);    
         } 
-// declare winner by setting a position and alert if player 2 won
+// declare winner by setting a position and set html player 2 won
         if($("#playerTwo").position().left > 1100){
-            alert("Player Two Winner");
+            $(".winnerPlacement").html("Player Two Wins");
         }
 	});
-    
+
 // set button to reload page
     $('#restartButton').click(function() {
     location.reload();
